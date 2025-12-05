49b6b6758db06fe251492e25c58011e97e3b14a0
root <root@Hashir.localdomain>
2025-12-05 19:29:34 +0500

    Initialize gh-pages branch

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/opt/my-book/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/opt/my-book/node_modules/infima/dist/css/default/default.css"),
+  require("/opt/my-book/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/opt/my-book/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/opt/my-book/src/css/custom.css"),
+];
